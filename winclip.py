5ea4becbcf746facb93c10bcdd9dca09e019d495
danghuyenvu <danghuyenvu516@gmail.com>
2025-12-22 01:04:24 +0700

    fixed bug that colapse anomaly score, modified to work on non-square images (not checked)

--- winclip.py
+++ winclip.py
@@ -12,7 +12,6 @@ class WinCLIP(nn.Module):
 	def __init__(self, states, templates, shots=0, option='AC'):
 		super().__init__()
 		self.model, _, self.preprocess = open_clip.create_model_and_transforms(MODEL_NAME, pretrained="winCLIP_ViT_B16P_laion400m.pt")
-		# self.model.load_state_dict(torch.load("winCLIP_ViT_B16P_laion400m.pt", map_location=DEVICE))
 		self.model.eval()
 		self.tokenizer = open_clip.get_tokenizer(MODEL_NAME)
 		self.window_mask = []
@@ -23,6 +22,7 @@ class WinCLIP(nn.Module):
 		self.template_level = templates
 		self.shots = shots
 		self.option = option
+		self.eval = False
 
 	@torch.no_grad()
 	def encode_image(self, img, windowmask=None, normalize=False):
@@ -65,9 +65,8 @@ class WinCLIP(nn.Module):
 				if self.visual.proj is not None:
 					cls = cls @ self.visual.proj
 
-				cls = F.normalize(cls, dim=-1)
+				cls /= cls.norm(dim=-1, keepdim=True)
 
-				# window_cls_list.append(cls.reshape((num_mask, 1, 1, -1)).permute(1, 0, 2, 3))
 				window_cls_list.append(cls)
 
 			#continue passing into transformer encoder the whole image
@@ -80,13 +79,15 @@ class WinCLIP(nn.Module):
 			if self.visual.proj is not None:
 				cls = cls @ self.visual.proj
 
-			cls = F.normalize(cls, dim=-1)
+			cls /= cls.norm(dim=-1, keepdim=True)
+
+			tokens /= tokens.norm(dim=-1, keepdim=True)
 
 			return window_cls_list, tokens, cls
 		else:
 			#getting the image scale feature then just get the cls token
 			features = self.model.encode_image(img)
-			return F.normalize(features, dim=-1) if normalize else features
+			return features / features.norm(dim=-1, keepdim=True)
 
 	@torch.no_grad()
 	def encode_text(self, object_name='all'):
@@ -96,6 +97,8 @@ class WinCLIP(nn.Module):
 		:param self: Doodoo
 		:param object_name: name of the query object
 		"""
+		if object_name[-1].isdigit():
+			object_name = object_name[:-1]
 		normal_states = [s.format(object_name) for s in self.state_level["normal"]]
 		anomaly_states = [s.format(object_name) for s in self.state_level["anomaly"]]
 
@@ -107,16 +110,11 @@ class WinCLIP(nn.Module):
 		normal_texts_f = self.model.encode_text(normal_texts_f)
 		abno_texts_f = self.model.encode_text(abno_texts_f)
 
-		normal_texts_f /= normal_texts_f.norm(dim=-1, keepdim=True)
-		abno_texts_f /= abno_texts_f.norm(dim=-1, keepdim=True)
-
 		normal_texts_f = torch.mean(normal_texts_f, dim=0, keepdim=True)
 		abno_texts_f = torch.mean(abno_texts_f, dim=0, keepdim=True)
 
-		normal_texts_f /= normal_texts_f.norm(dim=-1, keepdim=True)
-		abno_texts_f /= abno_texts_f.norm(dim=-1, keepdim=True)
-
 		text_features = torch.cat([normal_texts_f, abno_texts_f], dim=0)
+		text_features /= text_features.norm(dim=-1, keepdim=True)
 
 		return text_features
 
@@ -149,8 +147,6 @@ class WinCLIP(nn.Module):
 		:param masks: window mask, if none is passed then reference bank only
 		includes normal image features
 		"""
-		print("Begin generating reference bank...")
-
 		if masks is None:
 			#then expects the window_cls to be empty
 			for image in ref_list:
@@ -168,7 +164,7 @@ class WinCLIP(nn.Module):
 			#one more for image scale
 			self.reference_bank.append([])
 			for image in ref_list:
-				window_cls, tokens, image_cls = self.encode_image(image, masks)
+				window_cls, tokens, _ = self.encode_image(image, masks)
 
 				for index, cls in enumerate(window_cls):
 					cur_scale = [x for x in cls]
@@ -181,11 +177,10 @@ class WinCLIP(nn.Module):
 
 				self.reference_bank[-1].extend(token_list)
 
-		print("Reference bank generating finished.")
-
 	def calculate_text_anomaly_score(self, text_features, image_features, normal=False):
 		"""
 		Language-guided anomaly scoring function
+		Basically perform dot product between normalized tensors for cosine similarity
 		
 		:param self: Description
 		:param text_features: Text features extracted from Composition Promt Ensemble
@@ -196,7 +191,7 @@ class WinCLIP(nn.Module):
 			score_list = []
 			for x in image_features:
 				if x.shape[0] != 1:
-					x = x.unsqueeze(0)	
+					x = x.unsqueeze(0)
 				score = (100.0 * x@text_features.T).softmax(dim=-1)
 
 				if not normal:
@@ -320,74 +315,94 @@ class WinCLIP(nn.Module):
 		anomaly_map = torch.stack(anomaly_score_map, dim=0)
 		anomaly_map = torch.mean(anomaly_map, dim=0)
 		return anomaly_map
-	
 
 	@torch.no_grad()
-	def forward(self, object_name, image, ref_list, shot=0, option="AC"):
+	def forward(self, object_name, image, ref_list, shot=0, option="AC", out_size=(240, 240)):
 		"""
 		Main pipeline of the model
 		
 		:param self: Doodoo
 		:param object_name: name of the object needed to generate CPE
-		:param image: query image
+		:param image: query image (list of query image)
 		:param ref_list: list of reference images used for few-shots
 		:param shot: number of shot, auto=0 shot
 		:param option: 'AC' or 'AS' or None to run all evaluate metrics for both AS and AC (to be updated)
 		"""
 		with torch.no_grad():
 			text_features = self.encode_text(object_name)
-			image = image.to(DEVICE)
+			if isinstance(image, list):
+				image = [x.to(DEVICE) for x in image]
+			else:
+				image = [image.to(DEVICE)]
 			ref_list = [x.to(DEVICE) for x in ref_list]
 			if self.option == "AC":
 				if self.shots == 0:
 					#WinCLIP zero-shot AC basically applying CPE to CLIP image encoder and text encoder
-					image_features = self.encode_image(image)
-					return self.calculate_text_anomaly_score(text_features, image_features).detach().cpu().numpy()
+					image_features = [self.encode_image(x) for x in image]
+					score = self.calculate_text_anomaly_score(text_features, image_features)
+					
+					score = torch.stack(score)
+					score = torch.mean(score)
+					return score.detach().cpu().numpy()
 				else:
 					#adding aggregated vision-based anomaly score
+					scorelist = []
 					window_masks_1 = self.gen_window_mask(kernel_size=32).squeeze().to(DEVICE)
 					window_masks_2 = self.gen_window_mask(kernel_size=48).squeeze().to(DEVICE)
 					window_masks = [window_masks_1, window_masks_2]
 					self.gen_reference_bank(ref_list, window_masks)
-					window_cls, tokens, image_cls = self.encode_image(image, window_masks)
-					
-					text_image_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=False)
+					for x in image:
+						window_cls, tokens, image_cls = self.encode_image(x, window_masks)
+						
+						text_image_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=False)
 
-					vision_anomaly_map = self.calculate_visual_anomaly_score(tokens, window_cls, window_masks)
-					vision_anomaly_score = vision_anomaly_map.max()
+						vision_anomaly_map = self.calculate_visual_anomaly_score(tokens, window_cls, window_masks)
+						vision_anomaly_score = vision_anomaly_map.max()
 
-					AC_score = 0.5 * (text_image_score + vision_anomaly_score)
-					print(f"text: {text_image_score}")
-					print(f"vision: {vision_anomaly_score}")
-					return AC_score.detach().cpu().numpy()
+						AC_score = 0.5 * (text_image_score + vision_anomaly_score)
+						scorelist.append(AC_score)
+					scorelist = torch.stack(scorelist)
+					score = torch.mean(scorelist)
+					return score.detach().cpu().numpy()
 			else:
 				if self.shots == 0:
 					#zero-shot AS apply multi-scale aggregation score across pixels
 					window_masks_1 = self.gen_window_mask(kernel_size=32).squeeze().to(DEVICE)
 					window_masks_2 = self.gen_window_mask(kernel_size=48).squeeze().to(DEVICE)
 					window_masks = [window_masks_1, window_masks_2]
-					window_cls, tokens, image_cls = self.encode_image(image, window_masks)
+					scorelist = []
+					for x in image:
+						window_cls, tokens, image_cls = self.encode_image(x, window_masks)
 					
-					window_cls_list = []
-					for x in window_cls:
-						[window_cls_list.append(window) for window in x]
+						window_cls_list = []
+						for x in window_cls:
+							[window_cls_list.append(window) for window in x]
 
-					window_text_score = self.calculate_text_anomaly_score(text_features, window_cls_list, normal=True)
-					image_text_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=True)
+						window_text_score = self.calculate_text_anomaly_score(text_features, window_cls_list, normal=True)
+						image_text_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=True)
 
-					window_list = []
-					scale_indices = []
-					for x in window_masks:
-						x = x.T #(length, num_mask) -> (num_mask, length)
-						index = x.shape[0] if len(scale_indices) == 0 else scale_indices[-1] + x.shape[0]
-						scale_indices.append(index)
-						[window_list.append(window) for window in x]
+						window_list = []
+						scale_indices = []
+						for x in window_masks:
+							x = x.T #(length, num_mask) -> (num_mask, length)
+							index = x.shape[0] if len(scale_indices) == 0 else scale_indices[-1] + x.shape[0]
+							scale_indices.append(index)
+							[window_list.append(window) for window in x]
 
-					anomaly_score_map = self.textual_driven_anomaly_map(image_text_score, window_text_score=window_text_score, window_list= window_list, scale_indices=scale_indices)
+						anomaly_score_map = self.textual_driven_anomaly_map(image_text_score, window_text_score=window_text_score, window_list= window_list, scale_indices=scale_indices)
+						scorelist.append(anomaly_score_map)
+
+					scorelist = torch.stack(scorelist)
+					anomaly_score_map = torch.mean(scorelist, dim=0)
 
 					anomaly_map = anomaly_score_map.reshape(15, 15).unsqueeze(0)
 					anomaly_map = anomaly_map.unsqueeze(0)
-					anomaly_map = F.interpolate(anomaly_map, size=(240, 240), mode='bilinear', align_corners=False)
+
+					if self.eval:
+						anomaly_map = F.interpolate(anomaly_map, size=out_size, mode='bilinear', align_corners=False)
+						anomaly_map = anomaly_map.reshape(-1)
+					else:
+						anomaly_map = F.interpolate(anomaly_map, size=(240,240), mode='bilinear', align_corners=False)
 
 					ret_map = anomaly_map.squeeze().detach().cpu().numpy()
 					return ret_map
@@ -398,31 +413,42 @@ class WinCLIP(nn.Module):
 					window_masks = [window_masks_1, window_masks_2]
 					self.gen_reference_bank(ref_list, window_masks)
 
-					window_cls, tokens, image_cls = self.encode_image(image, window_masks)
+					maplist = []
+					for x in image:
+						window_cls, tokens, image_cls = self.encode_image(x, window_masks)
 					
-					window_cls_list = []
-					for x in window_cls:
-						[window_cls_list.append(window) for window in x]
+						window_cls_list = []
+						for x in window_cls:
+							[window_cls_list.append(window) for window in x]
 
-					window_text_score = self.calculate_text_anomaly_score(text_features, window_cls_list, normal=True)
-					image_text_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=True)
+						window_text_score = self.calculate_text_anomaly_score(text_features, window_cls_list, normal=True)
+						image_text_score = self.calculate_text_anomaly_score(text_features, image_cls, normal=True)
 
-					window_list = []
-					scale_indices = []
-					for x in window_masks:
-						x = x.T #(length, num_mask) -> (num_mask, length)
-						index = x.shape[0] if len(scale_indices) == 0 else scale_indices[-1] + x.shape[0]
-						scale_indices.append(index)
-						[window_list.append(window) for window in x]
+						window_list = []
+						scale_indices = []
+						for x in window_masks:
+							x = x.T #(length, num_mask) -> (num_mask, length)
+							index = x.shape[0] if len(scale_indices) == 0 else scale_indices[-1] + x.shape[0]
+							scale_indices.append(index)
+							[window_list.append(window) for window in x]
 
-					textual_score_map = self.textual_driven_anomaly_map(image_text_score, window_text_score=window_text_score, window_list= window_list, scale_indices=scale_indices)
+						textual_score_map = self.textual_driven_anomaly_map(image_text_score, window_text_score=window_text_score, window_list= window_list, scale_indices=scale_indices)
 
-					visual_score_map = self.calculate_visual_anomaly_score(tokens, window_cls, window_masks)
+						visual_score_map = self.calculate_visual_anomaly_score(tokens, window_cls, window_masks)
 
-					# final_map = 1.0 / (1.0 / visual_score_map + 1.0 / anomaly_score_map)
-					final_map = visual_score_map + textual_score_map
+						# final_map = 1.0 / (1.0 / visual_score_map + 1.0 / anomaly_score_map)
+						final_map = visual_score_map + textual_score_map
+						maplist.append(final_map)
+
+					final_map = torch.stack(maplist)
+					final_map = torch.mean(final_map, dim=0)
 					final_map = final_map.reshape(15, 15).unsqueeze(0)
 					final_map = final_map.unsqueeze(0)
-					ret_map = F.interpolate(final_map, size=(240, 240), mode='bilinear', align_corners=False)
+
+					if self.eval:
+						ret_map = F.interpolate(final_map, size=out_size, mode='bilinear', align_corners=False)
+						ret_map = ret_map.reshape(-1)
+					else:
+						ret_map = F.interpolate(final_map, size=(240,240), mode='bilinear', align_corners=False)
 
 					return ret_map.squeeze().detach().cpu().numpy()
